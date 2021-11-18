2594fb3a51bcef5737dd5cb01b236dcdb33e30dc
Zin Min Htet <zetminhtin@gmail.com>
2021-11-18 10:12:58 +0630

    Dark mode persistent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar, Footer, Routes } from './components/componentsExporter'
 const App = () => {
-  const [darkTheme, setDarkTheme] = useState(false)
+  const storedDarkMode = localStorage.getItem("DARK_MODE")
+  const [darkTheme, setDarkTheme] = useState(storedDarkMode)
+  useEffect(()=>{
+    localStorage.setItem("DARK_MODE", darkTheme)
+  },[darkTheme])
   return (
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen ">
